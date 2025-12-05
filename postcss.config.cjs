8b71e9adabc99a576174042def74728ab174849a
wwwketh62-cyber <you@example.com>
2025-12-05 05:17:28 -0500

    Fix PostCSS config for Vercel production build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,12 @@
 
 
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss,
+    autoprefixer
+  ]
 };
+
